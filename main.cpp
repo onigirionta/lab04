4fd527e0ef12d4bc2b0d19b5bf7455caa4df5e9f
Настя Мхитарян <onigirionta@gmail.com>
2017-06-01 10:08:15 +0300

    Solved conflict

--- main.cpp
+++ main.cpp
@@ -5,11 +5,8 @@
 #include <cctype>
 #include <cstring>
 #include <fstream>
-<<<<<<< HEAD
 #include <cstddef>
 #include <bitset>
-=======
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 #include "hex_print.h"
 #include "bin_print.h"
 using namespace std;
@@ -32,75 +29,6 @@ struct Student
     Student* groupHead;
 };
 
-<<<<<<< HEAD
-=======
-void partIV(){
-
-    char file_name[256];
-
-    cout << "Enter file name: ";
-    cin >> file_name;
-
-    if((strchr(file_name,'*') != NULL)||(strchr(file_name,'"') != NULL) ||
-        (strchr(file_name,'<') != NULL)||(strchr(file_name,'>') != NULL)||
-        (strchr(file_name,'?') != NULL)||(strchr(file_name,'|') != NULL)){
-        puts("Error! Incorrect file name");
-        return;
-    }
-
-
-    if((strchr(file_name,':') != NULL)&& 
-      (!(((strchr(file_name,':') - file_name + 1) == 2)&& 
-      (((strchr(file_name,'\\') - file_name + 1) == 3)|| 
-       isalpha(1) != 0)))){
-        cout << "Error! Incorrect file name";
-    } 
-
-    int a = strlen(file_name);
-
-    char * lastDotPnt = strrchr(file_name, '.');
-
-    if(lastDotPnt != NULL){
-        if(strcoll(lastDotPnt, ".txt")!=0){
-            strcat(file_name, ".txt");
-        }
-    }
-    else
-    {
-        strcat(file_name, ".txt");
-    }
-     
-    ifstream ifs(file_name);
-    int file_length = 0;
-
-    if (ifs) {
-        ifs.seekg (0, ifs.end);
-        file_length = ifs.tellg();
-        ifs.seekg (0, ifs.beg); 
-        char * file_content = new char[file_length];
-        ifs.read(file_content, file_length);
-        ifs.close();
-
-        cout << "Enter a string to search for (up to 255 characters):" << endl;
-        char substr[256];
-        cin >> substr;
-
-        int count = 0;
-        const char *tmp = file_content;
-        while(tmp = strstr(tmp, substr))
-        {
-           count++;
-           tmp++;
-        }
-        cout << "The string \"" << substr << "\" occures in the file " << file_name << " "  << count << " times." << endl;
-        delete[] file_content;
-        delete[] tmp;
-    }
-
-    delete[] lastDotPnt;
-}
-
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 void partII(){
     uint16_t first_op,second_op,result;
     char operation;
@@ -142,33 +70,22 @@ void partII(){
 }
 
 void partIII(){
-<<<<<<< HEAD
     Student group[3];
-=======
-    student group[3];
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 
     strcpy(group[0].name,"Daria");
     group[0].year_start = 2015;
     group[0].rate = 4.33;
     group[0].gender = 0;
     group[0].num_c = 2;
-<<<<<<< HEAD
     group[0].groupHead = NULL;
-=======
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 
 
     strcpy(group[1].name,"Ivan");
     group[1].year_start = 2014;
     group[1].rate = 3.55;
     group[1].gender = 1;
-<<<<<<< HEAD
     group[1].num_c = 3;
     group[1].groupHead = group;
-=======
-    group[1].num_c = 2;
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 
 
     strcpy(group[2].name,"Oleg");
@@ -176,7 +93,6 @@ void partIII(){
     group[2].rate = 4.76;
     group[2].gender = 0;
     group[2].num_c = 2;
-<<<<<<< HEAD
     group[2].groupHead = group;
 
 
@@ -252,34 +168,6 @@ void partIV(){
        isalpha(1) != 0)))){
         cout << "Error! Incorrect file name";
     } 
-=======
-}
-
-int main(){
-
-    cout << "Main menu. Choose subprogram:" << endl
-         << "2 -- Bitwise calcucator" << endl
-         << "3 -- Data allocation" << endl
-         << "4 -- Text search in file" << endl;
-    int c;
-    cin >> c;
-    switch(c)
-    {
-        case 2:
-            partII();
-            break;
-        case 3:
-            partIII();
-            break;
-        case 4:
-            partIV();
-            break;
-        default:
-            cout << "Wrong entry" << endl;
-    }
-    return 0;
-}
->>>>>>> 9fdae3fe75f85bb4abe9c8eebcf0630d3add5674
 
     int a = strlen(file_name);
 
